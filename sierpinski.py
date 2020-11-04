394500b1f95a8165484f8a1b369047d92f9afd1d
frcroth <frcroth@gmail.com>
2020-11-04 19:38:30 +0100

    Improve code

--- sierpinski.py
+++ sierpinski.py
@@ -1,67 +1,48 @@
 import turtle
 import random
-from math import *
+from math import sqrt
 
-turtle.setworldcoordinates(-0.1,-0.1,1.1,1.1)
-turtle.delay(0)
-turtle.hideturtle()
+def center(p1, p2):
+    return ((p1[0]+p2[0])/2, (p1[1]+p2[1])/2)
 
-A=[0,0]
-B=[1,0]
-C=[0.5,sqrt(3)/2]
-P0=[0.1,0.5]
+def draw():
+    turtle.setworldcoordinates(-0.1, -0.1, 1.1, 1.1)
+    turtle.delay(0)
+    turtle.hideturtle()
 
-turtle.setpos(A)
-turtle.dot(12,"blue")
-turtle.setpos(B)
-turtle.dot(12,"blue")
-turtle.setpos(C)
-turtle.dot(12,"blue")
-turtle.setpos(A)
+    A = [0, 0]
+    B = [1, 0]
+    C = [0.5, sqrt(3)/2]
+    P0 = [0.1, 0.5]
 
-turtle.penup()
+    turtle.setpos(A)
+    turtle.dot(12, "blue")
+    turtle.setpos(B)
+    turtle.dot(12, "blue")
+    turtle.setpos(C)
+    turtle.dot(12, "blue")
+    turtle.setpos(A)
 
-turtle.setpos(P0)
-turtle.dot(12,"red")
+    turtle.penup()
 
-def center(p1,p2):
-    return ( (p1[0]+p2[0])/2,(p1[1]+p2[1])/2)
+    turtle.setpos(P0)
+    turtle.dot(12, "red")
 
-i=1
-P=P0
-while i <= 7000:
-    wuerfel = random.randrange(3)
-    if wuerfel ==0:
-        P = center(P,A)
-    if wuerfel ==1:
-        P = center(P,B)
-    if wuerfel ==2:
-        P = center(P,C)
-    turtle.setpos(P)
-    turtle.dot(3)
-    i = i + 1
+    ITERATION_COUNT = 7000
 
-turtle.exitonclick()
+    P = P0
+    for i in range(ITERATION_COUNT):
+        dice = random.randrange(3)
+        if dice == 0:
+            P = center(P, A)
+        elif dice == 1:
+            P = center(P, B)
+        elif dice == 2:
+            P = center(P, C)
+        turtle.setpos(P)
+        turtle.dot(3)
 
+    turtle.exitonclick()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+if __name__ == "__main__":
+    draw()
